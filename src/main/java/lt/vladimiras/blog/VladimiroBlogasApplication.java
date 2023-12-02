a67ccac17f8aa53e37bce9d21639ca1f4829290c
Vladimiras <lioliap@gmail.com>
2023-12-02 17:01:07 +0200

    initial commit

---
+++ src/main/java/lt/vladimiras/blog/VladimiroBlogasApplication.java
@@ -0,0 +1,13 @@
+package lt.vladimiras.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VladimiroBlogasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VladimiroBlogasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lt/vladimiras/blog/VladimiroBlogasApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.vladimiras.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VladimiroBlogasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
